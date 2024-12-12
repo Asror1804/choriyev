3d8c3608d678d850e6a4f4fefb527fa103f4701f
Asror <m.o.asrorchoriyev79@gmail.com>
2024-12-12 23:32:13 +0500

    3-versiya 10 lik sanoq sistemasidan 16 lik sanoq sistemasiga otish

--- Program.cs
+++ Program.cs
@@ -9,9 +9,9 @@ class Program
         Console.Write("10-lik sanoq sistemasidagi sonni kiriting: ");
         int decimalNumber = int.Parse(Console.ReadLine());
 
-        // 8-lik sanoq sistemasiga o'tish
-        string octalNumber = Convert.ToString(decimalNumber, 8);
+        // 16-lik sanoq sistemasiga o'tish
+        string hexadecimalNumber = Convert.ToString(decimalNumber, 16).ToUpper();
 
-        Console.WriteLine($"8-lik sanoq sistemasidagi qiymati: {octalNumber}");
+        Console.WriteLine($"16-lik sanoq sistemasidagi qiymati: {hexadecimalNumber}");
     }
 }
