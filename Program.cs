cd94acaa7a89281387b4ece21b87a7abbe4780f7
Asror <m.o.asrorchoriyev79@gmail.com>
2024-12-12 22:58:37 +0500

    1-versiya 10 lik sanoq sistemasidan 2 lik sanoq sistemasiga otish

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+            using System;
+
+class Program
+        {
+            static void Main()
+            {
+                Console.Write("10-lik sanoq sistemasidagi sonni kiriting: ");
+                int decimalNumber = int.Parse(Console.ReadLine());
+
+                // 2-lik sanoq sistemasiga o'tish
+                string binaryNumber = Convert.ToString(decimalNumber, 2);
+
+                Console.WriteLine($"2-lik sanoq sistemasidagi qiymati: {binaryNumber}");
+            }
+        }
+
+
+
